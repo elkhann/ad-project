335c982a9399c932714dd456b2d96aa96fc6c47f
elkhann <emilkhann@gmail.com>
2018-06-11 23:46:25 +0300

    Создание главной страницы

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,23 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Home</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  
+  <div>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <v-carousel>
+          <v-carousel-item></v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12>
+          <h1>Home</h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
