17779182f158bf0b5aae3fde6dfb7125a6347741
elkhann <emilkhann@gmail.com>
2018-06-12 01:28:38 +0300

    Создание страницы формы

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,58 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>New Ad</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">New Ad</h1>
+        <v-form v-model="valid" ref="form" validation class="mb-3">
+          <v-text-field
+            name="title"
+            label="Ad title"
+            type="text"
+            required
+            v-model="title"
+            :rules="[v => !!v || 'Title is required']"
+          ></v-text-field>
+          <v-text-field
+            name="description"
+            label="Ad description"
+            type="text"
+            required
+            multi-line
+            v-model="description"
+            :rules="[v => !!v || 'Description is required']"
+          ></v-text-field>
+        </v-form>
+        <v-layout row class="mb-4">
+          <v-flex xs12>
+            <v-btn class="warning">
+              Upload
+              <v-icon right dark>cloud_upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <img src="" height="100px">
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch
+              color="primary"
+              label="Add to promo?"
+              v-model="promo"
+            ></v-switch>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-btn
+              :disabled="!valid"
+              class="success"
+              @click="createAd"
+            >Create Ad</v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +61,25 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        title: '',
+        description: '',
+        promo: '',
+        valid: false
+      }
+    },
+    methods: {
+      createAd () {
+        if (this.$refs.form.validate()) {
+          // logic
+          const ad = {
+            title: this.title,
+            description: this.description,
+            promo: this.promo
+          }
+          console.log(ad)
+        }
+      }
     }
   }
 </script>
